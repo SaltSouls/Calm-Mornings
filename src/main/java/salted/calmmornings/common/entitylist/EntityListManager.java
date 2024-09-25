6262e3ae98696c7b01c6fe3d2fb7923449920796
SaltSouls <beyondtwinsouls@gmail.com>
2024-09-24 18:40:44 -0700

    backported 1.21

--- src/main/java/salted/calmmornings/common/entitylist/EntityListManager.java
+++ src/main/java/salted/calmmornings/common/entitylist/EntityListManager.java
@@ -5,7 +5,6 @@ import com.google.common.collect.Sets;
 import net.minecraft.resources.ResourceLocation;
 import net.minecraft.world.entity.EntityType;
 import net.minecraft.world.entity.MobCategory;
-import net.minecraftforge.fml.ModList;
 import net.minecraftforge.registries.ForgeRegistries;
 import org.apache.commons.lang3.tuple.Pair;
 import org.apache.commons.lang3.tuple.Triple;
