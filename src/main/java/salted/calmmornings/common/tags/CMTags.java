5f8b48e857f8f6eb4ef38717c94e030c6d5a266a
SaltSouls <BeyondTwinSouls@gmail.com>
2025-01-24 02:27:56 -0800

    fixed entity tag crash

--- src/main/java/salted/calmmornings/common/tags/CMTags.java
+++ src/main/java/salted/calmmornings/common/tags/CMTags.java
@@ -1,14 +1,23 @@
 package salted.calmmornings.common.tags;
 
+import net.minecraft.core.Registry;
 import net.minecraft.core.registries.BuiltInRegistries;
+import net.minecraft.resources.ResourceKey;
+import net.minecraft.resources.ResourceLocation;
 import net.minecraft.tags.TagKey;
 import net.minecraft.world.entity.EntityType;
 import salted.calmmornings.CalmMornings;
 
 public class CMTags {
 
+    private static <T> ResourceKey<Registry<T>> createRegistryKey() {
+        return ResourceKey.createRegistryKey(new ResourceLocation("entity_type"));
+    }
+
+    public static final ResourceKey<Registry<EntityType<?>>> ENTITY_TYPE = createRegistryKey();
+
     public static final TagKey<EntityType<?>> DEFAULT_BLACKLIST = TagKey.create(
-            BuiltInRegistries.ENTITY_TYPE.key(),
+            ENTITY_TYPE,
             CalmMornings.resLoc("default_blacklist")
     );
 
