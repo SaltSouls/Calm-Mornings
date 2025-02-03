7420109d1a2e175379fa2361569626a612d6b27c
SaltSouls <BeyondTwinSouls@gmail.com>
2025-02-03 00:49:08 -0800

    datagen fix

--- src/main/java/salted/calmmornings/data/tags/CMEntityTags.java
+++ src/main/java/salted/calmmornings/data/tags/CMEntityTags.java
@@ -1,7 +1,6 @@
 package salted.calmmornings.data.tags;
 
-import net.minecraft.core.HolderLookup;
-import net.minecraft.data.PackOutput;
+import net.minecraft.data.DataGenerator;
 import net.minecraft.data.tags.EntityTypeTagsProvider;
 import net.minecraft.resources.ResourceLocation;
 import net.minecraft.tags.EntityTypeTags;
@@ -11,21 +10,19 @@ import org.jetbrains.annotations.Nullable;
 import salted.calmmornings.CalmMornings;
 import salted.calmmornings.common.tags.CMTags;
 
-import java.util.concurrent.CompletableFuture;
-
 public class CMEntityTags extends EntityTypeTagsProvider {
-    public CMEntityTags(PackOutput packOutput, CompletableFuture<HolderLookup.Provider> lookupProvider, @Nullable ExistingFileHelper existingFileHelper) {
-        super(packOutput, lookupProvider, CalmMornings.MODID, existingFileHelper);
+    public CMEntityTags(DataGenerator generator, @Nullable ExistingFileHelper existingFileHelper) {
+        super(generator, CalmMornings.MODID, existingFileHelper);
     }
 
     @Override
-    protected void addTags(HolderLookup.Provider provider) {
+    protected void addTags() {
         this.defaultBlacklist();
     }
 
     protected void defaultBlacklist() {
         tag(CMTags.DEFAULT_BLACKLIST)
-                .add(EntityType.PLAYER, EntityType.ENDER_DRAGON, EntityType.WITHER, EntityType.WARDEN, EntityType.GUARDIAN, EntityType.ELDER_GUARDIAN, EntityType.RAVAGER)
+                .add(EntityType.PLAYER, EntityType.ENDER_DRAGON, EntityType.WITHER, EntityType.GUARDIAN, EntityType.ELDER_GUARDIAN, EntityType.RAVAGER)
                 .addTag(EntityTypeTags.RAIDERS)
                 .addOptional(new ResourceLocation("sleep_tight", "bedbug"));
     }
